456adbccdc7e40b50a1d64218f402a6a98557229
Maxim Shapiro <maxim.shapira123@gmail.com>
2025-05-13 19:13:36 +0300

    put dig examples to readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dns-sniffer
+
+Test dig commands for reference:
+
+IPV4: dig @8.8.8.8 openai.com
+IPV6: dig @8.8.8.8 www.google.com AAAA
+CNAME: dig @8.8.8.8 www.youtube.com
+CNAME recursive: dig @8.8.8.8 www.gov.uk
+CNAME compressed: dig @8.8.8.8 www.microsoft.com
--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include "capture.h" 
 
 // this program captures DNS responses and prints the domain name and IP addresses
-// Test dig commands for reference:
-// IPV4: dig @8.8.8.8 openai.com
-// IPV6: dig @8.8.8.8 www.google.com AAAA
-// CNAME: dig @8.8.8.8 www.youtube.com
-// CNAME recursive: dig @8.8.8.8 www.gov.uk
-// CNAME compressed: dig @8.8.8.8 www.microsoft.com
 int main(int argc, char *argv[])
 {
     // Check if the user provided an interface name
